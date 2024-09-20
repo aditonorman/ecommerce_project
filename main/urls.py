62b40ddbe7756da59b3276cf9f11524cdca4b684
aditonorman <muhammad.raditya33@ui.ac.id>
2024-09-21 00:25:22 +0700

    update url

--- main/urls.py
+++ main/urls.py
@@ -17,9 +17,11 @@ urlpatterns = [
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
     path('register/', register, name='register'),
-    path('login/', login_user, name='login'),
-     path('logout/', logout_user, name='logout'),
+    path('accounts/login/', login_user, name='login'),  # Updated URL for login
+    path('logout/', logout_user, name='logout'),
 ]
+
+
 
 
 
